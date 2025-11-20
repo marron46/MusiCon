4a4528b3d8b9caebbd39040ee7caaf9095dd5c3b
hamaD9 <243158.kcsk@gmail.com>
2025-11-20 10:03:01 +0900

    marronさんですよ

--- src/main/java/servlet/LoginUser.java
+++ src/main/java/servlet/LoginUser.java
@@ -2,11 +2,13 @@ package servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/LoginUser")
 public class LoginUser extends HttpServlet {
@@ -24,12 +26,25 @@ public class LoginUser extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		String name = request.getParameter("name");
 		String pass = request.getParameter("pass");
-		
+
 		// ログイン処理の実行
-				User user = new User(name, pass);
-				LoginUserLogic logic = new LoginUserLogic();
-				boolean result = logic.execute(user);
-				
-				
+		User user = new User(name, pass);
+		LoginUserLogic logic = new LoginUserLogic();
+		boolean result = logic.execute(user);
+
+		// ログイン処理の成否によって処理を分岐
+		if (result) { // ログイン成功時
+			// セッションスコープにユーザーIDを保存
+			HttpSession session = request.getSession();
+			session.setAttribute("name", name);
+			// フォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("top.jsp");
+			dispatcher.forward(request, response);
+			System.out.print("でけた！");
+		} else { // ログイン失敗時
+			// リダイレクト
+			response.sendRedirect("LoginUser");
+			System.out.print("ろぐいんできない");
+		}
 	}
 }
