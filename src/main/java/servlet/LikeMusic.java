fb918e1899a8b097eebf485ef1d0f0766477e324
otosi <otosi@192.168.1.11>
2025-11-30 22:22:46 +0900

    ちゅかれた

---
+++ src/main/java/servlet/LikeMusic.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import model.logic.PlayMusicLogic;
+
+public class LikeMusic extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// リクエストパラメータを取得
+		request.setCharacterEncoding("UTF-8");
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		PlayMusicLogic logic = new PlayMusicLogic();
+		logic.likeMusic(id);
+
+		// 更新後、同じ曲ページへ戻る（リダイレクト）
+		response.sendRedirect("PlayMusic?id=" + id);
+
+	}
+
+}
