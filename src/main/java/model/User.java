fcd0cf1742f3534ade1562d9c438bc5a61f24d38
otosi <otosi@192.168.1.11>
2025-12-06 22:46:06 +0900

    ちょっとわけわかめ

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -9,6 +9,11 @@ public class User {//ユーザー情報格納
     public User() {}*/
 
     // コンストラクタ : フィールド設定
+	public User(){}
+	public User(String user_name,String user_pass){
+		this.user_name = user_name;
+		this.user_pass = user_pass;
+	}
 	public User(String user_name,String user_pass,int user_id){
 		this.user_name = user_name;
 		this.user_pass = user_pass;
